caf1283b2a40eece89f8734d5c3a9152a66d8dd5
Muhammad Kuswari <muh.kuswari10@gmail.com>
2021-04-12 10:24:45 +0800

    add assets & basic layout

---
+++ application/views/components/main/_sidebar.php
@@ -0,0 +1,41 @@
+<div class="main-sidebar sidebar-style-2">
+	<aside id="sidebar-wrapper">
+		<div class="sidebar-brand">
+			<a href="index.html">Inventory App</a>
+		</div>
+		<div class="sidebar-brand sidebar-brand-sm">
+			<a href="index.html">IA</a>
+		</div>
+		<ul class="sidebar-menu">
+			<li class="menu-header">Dashboard</li>
+			<li class="active">
+				<a class="nav-link" href="credits.html"><i class="fas fa-fire"></i> <span>Dashboard</span></a>
+			</li>
+			<li class="menu-header">Master Data</li>
+			<li class="dropdown">
+				<a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-box-open"></i> <span>Kelola Barang</span></a>
+				<ul class="dropdown-menu">
+					<li><a class="nav-link" href="layout-default.html">Item</a></li>
+					<li><a class="nav-link" href="layout-transparent.html">Kategori</a></li>
+					<li><a class="nav-link" href="layout-top-navigation.html">Satuan</a></li>
+				</ul>
+			</li>
+			<li><a class="nav-link" href="blank.html"><i class="fas fa-truck-moving"></i> <span>Kelola Supplier</span></a></li>
+			<li><a class="nav-link" href="blank.html"><i class="fas fa-users"></i> <span>Kelola Customer</span></a></li>
+			<li class="dropdown">
+				<a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i> <span>Kelola Transaksi</span></a>
+				<ul class="dropdown-menu">
+					<li><a class="nav-link" href="bootstrap-alert.html">Barang Masuk</a></li>
+					<li><a class="nav-link" href="bootstrap-badge.html">Barang Keluar</a></li>
+				</ul>
+			</li>
+			<li class="menu-header">Laporan</li>
+			<li><a class="nav-link" href="blank.html"><i class="fas fa-file"></i> <span>Rekap Transaksi</span></a></li>
+			<li><a class="nav-link" href="blank.html"><i class="fas fa-file"></i> <span>Rekap Supplier</span></a></li>
+			<li class="menu-header">Pengguna</li>
+			<li><a class="nav-link" href="blank.html"><i class="fas fa-users"></i> <span>Kelola Users</span></a></li>
+			<li><a class="nav-link" href="credits.html"><i class="fas fa-user-circle"></i> <span>Profil Saya</span></a></li>
+		</ul>
+
+	</aside>
+</div>
