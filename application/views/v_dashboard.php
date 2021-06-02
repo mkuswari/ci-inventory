75c6256ede2a2990f9fe984f8f5e25b3219a6696
Muhammad Kuswari <muh.kuswari10@gmail.com>
2021-06-03 01:53:25 +0800

    set base url to dynamic url

--- application/views/v_dashboard.php
+++ application/views/v_dashboard.php
@@ -23,64 +23,72 @@
 					</div>
 					<div class="row">
 						<div class="col-lg-3 col-md-6 col-sm-6 col-12">
-							<div class="card card-statistic-1">
-								<div class="card-icon bg-primary">
-									<i class="fas fa-users"></i>
-								</div>
-								<div class="card-wrap">
-									<div class="card-header">
-										<h4>Jumlah Admin</h4>
+							<a href="<?= base_url("user") ?>">
+								<div class="card card-statistic-1">
+									<div class="card-icon bg-primary">
+										<i class="fas fa-users"></i>
 									</div>
-									<div class="card-body">
-										<?= $total_admins ?>
+									<div class="card-wrap">
+										<div class="card-header">
+											<h4>Jumlah Admin</h4>
+										</div>
+										<div class="card-body">
+											<?= $total_admins ?>
+										</div>
 									</div>
 								</div>
-							</div>
+							</a>
 						</div>
 						<div class="col-lg-3 col-md-6 col-sm-6 col-12">
-							<div class="card card-statistic-1">
-								<div class="card-icon bg-danger">
-									<i class="fas fa-truck-moving"></i>
-								</div>
-								<div class="card-wrap">
-									<div class="card-header">
-										<h4>Jumlah Supplier</h4>
+							<a href="<?= base_url("supplier") ?>">
+								<div class="card card-statistic-1">
+									<div class="card-icon bg-danger">
+										<i class="fas fa-truck-moving"></i>
 									</div>
-									<div class="card-body">
-										<?= $total_suppliers ?>
+									<div class="card-wrap">
+										<div class="card-header">
+											<h4>Jumlah Supplier</h4>
+										</div>
+										<div class="card-body">
+											<?= $total_suppliers ?>
+										</div>
 									</div>
 								</div>
-							</div>
+							</a>
 						</div>
 						<div class="col-lg-3 col-md-6 col-sm-6 col-12">
-							<div class="card card-statistic-1">
-								<div class="card-icon bg-warning">
-									<i class="fas fa-users"></i>
-								</div>
-								<div class="card-wrap">
-									<div class="card-header">
-										<h4>Jumlah Customers</h4>
+							<a href="<?= base_url("customer") ?>">
+								<div class="card card-statistic-1">
+									<div class="card-icon bg-warning">
+										<i class="fas fa-users"></i>
 									</div>
-									<div class="card-body">
-										<?= $total_customers ?>
+									<div class="card-wrap">
+										<div class="card-header">
+											<h4>Jumlah Customers</h4>
+										</div>
+										<div class="card-body">
+											<?= $total_customers ?>
+										</div>
 									</div>
 								</div>
-							</div>
+							</a>
 						</div>
 						<div class="col-lg-3 col-md-6 col-sm-6 col-12">
-							<div class="card card-statistic-1">
-								<div class="card-icon bg-success">
-									<i class="fas fa-circle"></i>
-								</div>
-								<div class="card-wrap">
-									<div class="card-header">
-										<h4>Jumlah Barang</h4>
+							<a href="<?= base_url("item") ?>">
+								<div class="card card-statistic-1">
+									<div class="card-icon bg-success">
+										<i class="fas fa-circle"></i>
 									</div>
-									<div class="card-body">
-										<?= $total_items ?>
+									<div class="card-wrap">
+										<div class="card-header">
+											<h4>Jumlah Barang</h4>
+										</div>
+										<div class="card-body">
+											<?= $total_items ?>
+										</div>
 									</div>
 								</div>
-							</div>
+							</a>
 						</div>
 					</div>
 				</section>
